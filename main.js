7e19bf20c720fe434d69a5d5c687aed6a1f7d1bf
YVIS TRINDADE DO NASCIMENTO <yvis.tnascimento@senacsp.edu.br>
2025-02-20 21:45:06 -0300

    estrutura do projeto

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 /**
- * Processo principal 
- * Estudo do CRUD do mongoDB
+ * Processo principal
+ * Estudo do CRUD com MongoDB
  */
 
-//importação do modulo de conexão (database.js)
-const {conectar, desconectar} = require('./database.js')
+// importação do módulo de coneção (database.js)
+const { conectar, desconectar } = require('./database')
 
-//execução de aplicação
+// importação do modelo de dados de clientes
+const clienteModel = require('./src/models/Clientes')
+
+// CRUD Create (função para adicionar um novo cliente)
+const criarCliente = async (nomeCli, foneCli) => {
+    try {
+        const novoCliente = new clienteModel(
+            {
+                nomeCliente: nomeCli,
+                foneCliente: foneCli
+            }
+        )
+        // a linha abaixo salva os dados do cliente no banco
+        await novoCliente.save()
+        console.log("Cliente adicionado com sucesso.")
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// execução da aplicação
 const app = async () => {
     await conectar()
+    await criarCliente("Jean Andrade", "11912341234")
+    await criarCliente("Gabriel Yago", "11912341234")
     await desconectar()
 }
+
 console.clear()
 app()
