b930b52671f2c4c1532fba86c0d38170c3782bbe
YVIS TRINDADE DO NASCIMENTO <yvis.tnascimento@senacsp.edu.br>
2025-02-21 22:15:45 -0300

    estrutura do projeto

--- main.js
+++ main.js
@@ -9,19 +9,78 @@ const { conectar, desconectar } = require('./database')
 // importação do modelo de dados de clientes
 const clienteModel = require('./src/models/Clientes')
 
+// importação do pacote string-similarity para aprimorar a busca por nome
+const stringSimilarity = require('string-similarity')
+
 // CRUD Create (função para adicionar um novo cliente)
-const criarCliente = async (nomeCli, foneCli) => {
+const criarCliente = async (nomeCli, foneCli, cpfCli) => {
     try {
         const novoCliente = new clienteModel(
             {
                 nomeCliente: nomeCli,
-                foneCliente: foneCli
+                foneCliente: foneCli,
+                cpf: cpfCli
             }
         )
         // a linha abaixo salva os dados do cliente no banco
         await novoCliente.save()
         console.log("Cliente adicionado com sucesso.")
 
+    } catch (error) {
+        // tratamento de exceções especificas
+        if (error.code = 11000) {
+            console.log(`Erro: O CPF ${cpfCli} já está cadastrado`)
+        } else {
+            console.log(error)
+        }
+
+    }
+}
+
+// CRUD Read - Função para listar todos os clientes cadastrados
+const listarClientes = async () => {
+    try {
+        // a linha abaixo lista todos os clientes cadastrados
+        const clientes = await clienteModel.find().sort(
+            {
+                nomeCliente: 1
+            }
+        )
+        console.log(clientes)
+    } catch (error) {
+        console.log(error)
+    }
+}
+// CRUD Read - Função para buscar um cliente especifico
+const buscarCliente = async (nome) => {
+    try {
+        // find() buscar
+        // nomeCliente: new RegExp(nome) filtro pelo nome (partes que contenham (expressão regular))
+        // 'i' insensitive (ignorar letras maiúsculas ou minúsculas)
+        const cliente = await clienteModel.find(
+            {
+                nomeCliente: new RegExp(nome, 'i')
+            }
+        )
+        // calcular a similaridade entre os nomes retornados e o nome pesquisado
+        const nomesClientes = cliente.map(cliente => cliente.nomeCliente)
+
+        // validação (se não existir o cliente pesquisado)
+        if (nomesClientes.length === 0) {
+            console.log("Cliente não cadastrado")
+        } else {
+            const match = stringSimilarity.findBestMatch(nome, nomesClientes)
+            // cliente com melhor similaridade
+            const melhorCliente = cliente.find(cliente => cliente.nomeCliente === match.bestMatch.target)
+            // formatação da data
+            const clienteFormulario = {
+                nomeCliente: melhorCliente.nomeCliente,
+                foneCliente: melhorCliente.foneCliente,
+                cpf: melhorCliente.cpf,
+                dataCadastro: melhorCliente.dataCadastro.toLocaleDateString('pt-BR')
+            }
+            console.log(clienteFormulario)
+        }
     } catch (error) {
         console.log(error)
     }
@@ -30,8 +89,19 @@ const criarCliente = async (nomeCli, foneCli) => {
 // execução da aplicação
 const app = async () => {
     await conectar()
-    await criarCliente("Jean Andrade", "11912341234")
-    await criarCliente("Gabriel Yago", "11912341234")
+    // CRUD - Create
+    //await criarCliente("Neimar Junior", "11912345678", "123.456.789-02")
+    //await criarCliente("Roberto Carlos", "11912345678", "123.456.789-03")
+    //await criarCliente("Ronaldo Fenomeno", "11912345678", "123.456.789-04")
+    //await criarCliente("Ronaldinho Gaucho", "11912345678", "123.456.789-05")
+    //await criarCliente("Carlito Tevez", "11912345678", "123.456.789-06")
+
+    // CRUD - Read (Exemplo 1 - listar clientes)
+    // await listarClientes()
+
+    // CRUD - Read (Exemplo 2 - buscar cliente)
+    await buscarCliente("jean")
+
     await desconectar()
 }
 
--- src/models/Clientes.js
+++ src/models/Clientes.js
@@ -2,20 +2,32 @@
  * Modelo de dados (coleção)
  * Clientes
  */
- 
+
 // importação da biblioteca
 const { model, Schema } = require('mongoose')
- 
+
 // criação da estrutura de dados ("coleção") que será usada no banco
 const clienteSchema = new Schema({
+
     nomeCliente: {
         type: String
     },
     foneCliente: {
         type: String
     },
-}, {versionKey: false})
- 
+
+    cpf: {
+        type: String,
+        unique: true,
+        index: true
+    },
+
+    dataCadastro: {
+    type: Date,
+    default: Date.now
+}
+}, {versionKey: false })
+
 // importação do modelo de dados
 // obs: Clientes será o nome da coleção
 module.exports = model('Clientes', clienteSchema)
